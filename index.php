562f41da989db969a98c084293a65cc233997d5c
Albert Stepantsev <test@pr-of-it.ru>
2016-10-24 20:53:29 +0300

    Урок 3 ДЗ

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ $news = \App\Model\Article::findAll();
 $view = new \App\View();
 $view->news = $news;
 
-$view->foo = 12;
-
 $html = $view->render(__DIR__ . '/templates/index.php');
 $html = str_replace('без автора', 'автора нет', $html);
 echo $html;
